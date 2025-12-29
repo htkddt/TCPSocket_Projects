9d06a9cc3d4c639bd0c6db0983c874260e5b6878
htkddt <huynhtuankietddt@gmail.com>
2025-12-30 01:04:18 +0700

    Modified the path of run Window Batch File

--- Automation_Remote/Local/run.bat
+++ Automation_Remote/Local/run.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "python C:\Users\tuanng4x\Workspace\Tickets\TCP_AutomationTool\Local\in-run_tst.py %1 bdd_test"
+start cmd /k "python D:\A_Projects\TCPSocket_Projects\Automation_Remote\Local\in-run_tst.py %1 bdd_test"
